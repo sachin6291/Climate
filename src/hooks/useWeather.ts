8085b6891614ee5686e3fd48ff253b106a06e771
Sachin Tekchandani <sachin6291@gmail.com>
2024-04-16 05:08:24 +0100

    http to https

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -54,7 +54,7 @@ const useWeather=()=>{
         setNotFound(false)
 
         try {
-            const geoUrl =`http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+            const geoUrl =`https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
             
             const {data} = await axios.get(geoUrl)
 
